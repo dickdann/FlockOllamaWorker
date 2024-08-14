ec896d91e8f63066a7861abf064215ca5aa2a29d
dickdann <richard@smartmoves.co.nz>
2024-08-15 09:22:31 +1200

    packages

--- install.js
+++ install.js
@@ -3,9 +3,13 @@ const path = require('path');
 
 const sourceDir = path.join(__dirname, '..', 'flockollama-worker');
 const targetDir = process.cwd();
-console.log("Running post install step");
-fs.copySync(path.join(sourceDir, '.env.example'), path.join(targetDir, '.env'));
-fs.copySync(path.join(sourceDir, 'run.js'), path.join(targetDir, 'flockollama-runner.js'));
+console.log(`Running post install step.  TargetDirectory is ${targetDir} sourceDir is ${sourceDir}`);
 
-console.log("Post install step complete.  To run the worker, execute 'node flockollama-runner.js'");
-console.log("Alternatively use: pm2 start flockollama-runner.js --name flockollama-worker");
+setTimeout(() => {
+    console.log("Copying files...");
+    fs.copySync(path.join(sourceDir, '.env.example'), path.join(targetDir, '.env'));
+    fs.copySync(path.join(sourceDir, 'run.js'), path.join(targetDir, 'flockollama-runner.js'));
+    console.log("Post install step complete.  To run the worker, execute 'node flockollama-runner.js'");
+    console.log("Alternatively use: pm2 start flockollama-runner.js --name flockollama-worker");
+}, 3000);
+
